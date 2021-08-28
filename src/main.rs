7139fb3f3d3d4e0b9babfce260d6d472573e2259
zhouli02 <1310537426@qq.com>
2021-08-28 16:41:07 +0800

    trait

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,43 @@
-fn main(){
+
+struct Student {
+    name: String,
+    age: u32,
+}
+
+struct Teacher {
+    name: String,
+    sub: String,
+    age: u32,
+}
+
+trait SchoolName {
+    fn get_school_name(&self) ->String {            
+        String::from("csust")
+    }
+}
+
+impl SchoolName for Teacher{
+
+}
+
+impl SchoolName for Student{
     
-    let p=1;
-    let y = p*2;
-    let z = p*3;
-    let v = p*4;
-    let w = p*5;
-    println!("{}    {}    {}   {}",y,z,v,w);
+}
+
+fn main(){
+    let stu1 = Student {
+        name : "zl".to_string(),
+        age: 21,
+    };
+    let tea1 = Teacher {
+        name : "tjf".to_string(),
+        sub: "math".to_string(),
+        age: 50,
+    };
+    let stu1_school_name=stu1.get_school_name();
+    let tea1_school_name=tea1.get_school_name();
+    let sum_name = format!("student's school :{}  and teacher's school : {}",stu1_school_name,tea1_school_name);
+    println!("{}",sum_name.len());
+    println!("{}",sum_name);
+    println!("{}   {}",stu1_school_name,tea1_school_name);
  }
